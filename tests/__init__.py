7e00791ed8e85074cb8fc05dceaf6d81b1e7ca66
SJShaw <SJShaw@users.noreply.github.com>
2013-05-30 20:11:15 +1000

    bringing up to date

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+"""Unit tests
+
+Usage: From project's root directory:
+$ python tests
+
+or
+
+$ python -m unittest tests
+
+"""
+
+from naming import *
+from indents import *
+from length import *
+from braces import *
---
+++ tests/indents.py
@@ -0,0 +1,21 @@
+import unittest
+import simpatico
+
+class TestIndents(unittest.TestCase):
+    EXPECTED = [(3, 'Indentation error (expected 0, got 4)'),
+                (7, 'Indentation error (expected 4, got 8)'),
+                (11, 'Indentation error (expected 8, got 7)'),
+                (17, 'Indentation error (expected 12, got 8)'),
+                (21, 'Indentation error (expected 8, got 4)'),
+                (27, 'Indentation error (expected 12, got 16)'),
+                (28, 'Indentation error (expected 12, got 16)'),
+                (30, 'Indentation error (expected 12, got 8)'),
+                (40, 'Indentation error (expected 12, got 8)'),
+                (44, 'Indentation error (expected 0, got 4)')]
+    def runTest(self):
+        lines = simpatico.get_lines('tests/files/indents.c')
+        errors = simpatico.check_indents(lines)
+        self.assertItemsEqual(errors, self.EXPECTED)
+
+if __name__ == "__main__":
+    unittest.main()
