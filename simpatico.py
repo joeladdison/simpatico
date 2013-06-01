916ae033c60c08d47e57474de8eef1c129b80708
SJShaw <SJShaw@users.noreply.github.com>
2013-06-01 18:21:15 +1000

    extended error reporting/generation changed token movement

--- simpatico.py
+++ simpatico.py
@@ -24,8 +24,8 @@ class Type(object):
     (   ERROR_TYPE, DEFINE, INCLUDE, COMMENT, NEWLINE, COMMA, LBRACE, RBRACE,
         LPAREN, RPAREN, MINUS, BINARY_OPERATOR, LOGICAL_OPERATOR, STAR,
         AMPERSAND, TYPE, CREMENT, IGNORE, KW_EXTERN, BREAK, FOR, SWITCH, CASE,
-        STRUCT, CONTINUE, TYPEDEF, RETURN
-    ) = range(27)
+        STRUCT, CONTINUE, TYPEDEF, RETURN, UNKNOWN, CONSTANT, WHILE, DO
+    ) = range(31)
 
 class Fragment(object):
     """ This is where we start getting funky with building the structure of
@@ -61,7 +61,7 @@ class Word(object):
     def get_string(self):
         return "".join(self.line)
 
-    def get_start_position(self):
+    def get_position(self):
         return self.start
 
     def get_spacing_left(self):
@@ -83,7 +83,8 @@ class Word(object):
         #prepare thyself for many, many elifs
         if line.lower() == "#define":
             self.type = Type.DEFINE
-        elif line.lower() == "#include":
+        elif line.lower() in ["#include", "#ifdef", "#ifndef", "#endif",
+                "#undef"]:
             self.type = Type.INCLUDE
         elif line == "\t":
             self.type = Type.COMMENT
@@ -121,6 +122,10 @@ class Word(object):
             self.type = Type.BREAK
         elif line == "for":
             self.type = Type.FOR
+        elif line == "do":
+            self.type = Type.DO
+        elif line == "while":
+            self.type = Type.WHILE
         elif line == "switch":
             self.type = Type.SWITCH
         elif line == "case":
@@ -133,6 +138,10 @@ class Word(object):
             self.type = Type.TYPEDEF
         elif line == "return":
             self.type = Type.RETURN
+        elif line[0] == '"' or line[0] == "'" or line.isdigit():
+            self.type = Type.CONSTANT
+        else:
+            self.type = Type.UNKNOWN #variables and externally defined types
 
     def get_type(self):
         return self.type
@@ -278,44 +287,131 @@ class Tokeniser(object):
                     self.add_to_word(c, n)
                     self.end_word()
             else:
-                if c == '#' and not self.tokens:
-                    self.hash_line = True
                 self.add_to_word(c, n)
 
     def get_tokens(self):
         return self.tokens
 
-class Styler:
+class Errors(object):
+    (IF, ELSE, ELSEIF, RUNON, FUNCTION, GLOBALS, VARIABLE, TYPE,
+            DEFINE) = range(9)
+    """Everyone's favourite"""
+    def __init__(self):
+        self.naming_d = {}
+        self.whitespace_d = {}
+        self.comments_d = {}
+        self.braces_d = {}
+        self.line_length_d = {}
+        self.func_length_d = {}
+        self.total = 0
+
+    def naming(self, token, name_type):
+        self.total += 1
+        msg = "WHOOPS"
+        if name_type == Styler.TYPE:
+            msg = " misnamed, types should be NamedLikeThis"
+        elif name_type == Styler.FUNCTION:
+            msg = " misnamed, functions should be named_like_this"
+        elif name_type == Styler.DEFINE:
+            msg = " misnamed, #defines should be NAMED_LIKE_THIS"
+        elif name_type == Styler.VARIABLE:
+            msg = " misnamed, variables should be namedLikeThis"
+        self.naming_d[token.get_line_number()] = "[NAMING] " + \
+                token.get_string() + msg
+
+    def whitespace(self, token, expected):
+        self.total += 1
+        self.whitespace_d[token.get_line_number()] = "[WHITESPACE] At \
+                position %d expected %d whitespace, had %d" % (
+                token.get_position(), expected, token.get_spacing_left())
+
+    def line_length(self, line_number, length):
+        self.total += 1
+        self.line_length_d[line_number] = "line is %d chars long" % length
+
+    def func_length(self, line_number, length):
+        self.total += 1
+        self.func_length_d[line_number] = "[OVERALL] Function length of %d is \
+                over the maximum of 50" % length
+
+    def braces(self, token, error_type):
+        self.total += 1
+        msg = "WHOOPS"
+        if error_type == Errors.IF:
+            msg = ", if braces should look like: if (cond) {"
+        elif error_type == Errors.ELSE:
+            msg = ", else braces should look like: } else {"
+        elif error_type == Errors.ELSEIF:
+            msg = ", else if braces should look like: } else if (cond) {"
+        elif error_type == Errors.RUNON:
+            msg = ", braces should be the last character on the line"
+        self.braces[token.get_line_number()] = "[BRACES] at position " + \
+                token.get_position() + msg
+
+    def comments(self, line_number, error_type):
+        self.total += 1
+        msg = "WHOOPS"
+        if error_type == Errors.FUNCTION:
+            msg = "Functions should be preceeded by explanatory comments"
+        elif error_type == Errors.GLOBALS:
+            msg = "Global variables should be commented"
+        self.comments[line_number] = msg
+
+    def get(self, line_number):
+        result = []
+        for error_type in [self.braces_d, self.whitespace_d,
+                self.line_length_d, self.naming_d, self.func_length_d,
+                self.comments_d]:
+            result.extend(error_type.get(line_number, []))
+        return result
+
+    def __repr__(self):
+        counts = [len(error_type.keys()) for error_type in [
+                self.braces_d, self.whitespace_d,
+                self.line_length_d, self.naming_d, self.func_length_d,
+                self.comments_d]]
+        for i in range(len(counts)):
+            if counts[i] > 5:
+                counts[i] = 5
+        return "%d total errors found, " % self.total + \
+                "B:%d W:%d C:%d N:%d O:%d L:%d (capped at 5 per type)" % \
+                tuple(counts)
+
+class Styler(object):
+    MAX = False
     """ Where style violations are born """
     def __init__(self, filename, verbose = False, output_file = False):
         #some setup
-        self.errors = {}
+        self.errors = Errors()
 
         #quick run for line lengths
         line_number = 0
         self.infile = open(filename, "r")
         for line in self.infile:
             if len(line) > 79:
-                self.errors[line_number] = \
-                        "[LINE-LENGTH] Line is %d characters long" % len(line)
-        self.comment_lines = {}
-        #then the guts of it all
-        self.process(filename)
-
+                self.errors.line_length(line_number, len(line))
         self.infile.close()
+        self.position = 0
+        #then the guts of it all
+        tokeniser = Tokeniser(filename)
+        self.tokens = tokeniser.get_tokens()
+        self.current_token = self.tokens[self.position]
+        try:
+            self.process(filename)
+        except IndexError as e:
+            #that'd be us finished
+            pass
 
         if output_file:
             self.write_output_file(filename)
 
         if verbose:
-            self.print_errors()
+            print self.errors
 
-    def print_errors(self):
-        #and a little message for people, this could be extended
-        if self.errors:
-            print "style violations found"
-        else:
-            print "celebrate"
+    def match(self):
+        self.position += 1
+        #unsafe, deliberately so
+        self.current_token = self.tokens[self.position]
 
     def write_output_file(self, filename):
         """go over the file and insert messages when appropriate"""
@@ -323,25 +419,163 @@ class Styler:
         outf = open(filename+".style", "w")
         infile = open(filename, "r")
         for line in infile:
-            if len(line.expandtabs()) > 79:
-                outf.write("[LINE-LENGTH] Line is %d characters long" \
-                        % len(line))
-            outf.writelines(self.errors.get(line_number, []))
+            outf.writelines(self.errors.get(line_number))
         infile.close()
         outf.close()
 
     def process(self, filename):
-        tokeniser = Tokeniser(filename)
-        tokens = tokeniser.get_tokens()
-        constructs = []
-        current_construct = []
-        for i in xrange(len(tokens)):
-            token = tokens[i]
+        """ There's an assumption here that the code compiles to start with.
+        """
+        i = self.position
+        while True:
+            token = self.current_token
+            tokentype = token.get_type()
+            #only checking things which can start statements in global space
+            if token.type == Type.INCLUDE:
+                #just strip these out, we don't care
+                while self.current_token.get_type() != Type.NEWLINE:
+                    self.match()
+            elif token.type == Type.DEFINE:
+                self.match()
+                self.check_define()
+            elif token.type == Type.TYPE:
+                self.match()
+                self.check_declaration()
+            elif token.type == Type.IGNORE:
+                self.match()
+                self.check_declaration()
+            elif token.type == Type.LBRACE:
+                self.match()
+                self.check_block()
+            elif token.type == Type.NEWLINE:
+                self.match()
+                #skip leading blank lines
+                #TODO maybe violate it
+                continue
+            elif token.type == Type.COMMENT:
+                self.match()
+                self.check_comment()
+            elif token.type == Type.UNKNOWN:
+                self.match()
+                self.check_statement()
+            else:
+                self.match()
+                print "found an awkward type in global space:", token
+
+    def check_whitespace(self, token, expected, is_strict = True):
+        if is_strict:
+            if token.get_spacing_left() != expected:
+                self.errors.whitespace(token, expected)
+        else:
+            if token.get_spacing_left() <= expected:
+                self.errors.whitespace(token, expected)
+
+    def check_naming(self, token, name_type = Errors.VARIABLE):
+        name = token.get_string()
+        if name_type == Errors.VARIABLE:
+            if "_" in name or len(name) == 1 and name.isupper():
+                self.errors.naming(token, name_type)
+        elif name_type == Errors.FUNCTION:
+            for c in name:
+                if c.isupper():
+                    self.errors.naming(token, name_type)
+                    break
+        elif name_type == Errors.TYPE:
+            if "_" in name or not name[0].isupper():
+                self.errors.naming(token, name_type)
+        elif name_type == Errors.DEFINE:
+            if not name.isupper():
+                self.errors.naming(token, name_type)
+        else:
+            print "check_naming(): unknown naming type given: token=", token
+
+    def check_for(self, depth = 0):
+        print "check_for(): not implemented"
+
+    def check_while(self, depth = 0):
+        print "check_while(): not implemented"
+
+    def check_do(self, depth = 0):
+        print "check_do(): not implemented"
+
+    def check_return(self, depth = 0):
+        print "check_return(): not implemented"
+
+    def check_switch(self, depth = 0):
+        print "check_switch(): not implemented"
+
+    def check_case(self, depth = 0):
+        print "check_case(): not implemented"
+
+    def check_statement(self, depth = 0):
+        print "check_statement(): not implemented"
+
+    def check_comment(self, depth = 0):
+        print "check_comment(): not implemented"
+
+    def check_block(self, depth = 0):
+        print "check_for(): not implemented"
+
+    def check_define(self, depth = 0):
+        print "check_define(): not implemented"
+
+    def check_paren(self, depth = 0):
+        print "check_paren(): not implemented"
+
+    def check_declaration(self, depth = 0):
+        terminator = None
+        expected_indent = depth * 4
+        while True:
+            token = self.current_token
+            if token.type == terminator:
+                self.match()
+                return
+            # check indentation
+            self.check_whitespace(token, expected_indent)
+            # continue checking
+            if token.type == Type.STAR:
+                #found a pointer declaration
+                self.check_whitespace(token, 1, Styler.MAX)
+                name = self.tokens[i+1] #TODO check if newline
+            if token.type == Type.UNKNOWN:
+                #sounds like a variable name
+                self.match()
+                pass #TODO
+            if token.type == Type.IGNORE:
+                self.match()
+                pass
+            if token.type == Type.RETURN:
+                self.match()
+                self.check_return()
+            elif token.type == Type.FOR:
+                self.match()
+                self.check_for()
+            elif token.type == Type.CREMENT:
+                self.match()
+                self.check_statement()
+            elif token.type == Type.LPAREN:
+                self.match()
+                self.check_paren()
+            elif token.type == Type.WHILE:
+                self.match()
+                self.check_while()
+            elif token.type == Type.DO:
+                self.match()
+                self.check_do()
+            elif token.type == Type.SWITCH:
+                self.match()
+                self.check_switch()
+            elif token.type == Type.STAR:
+                self.match()
+                self.check_statement()
+            else:
+                print "check_declaration() found unknown type:", token
+                self.match()
 
 if __name__ == '__main__':
     for i in range(1, len(sys.argv)):
         if sys.argv[i].strip():
             print 'Parsing %s...' % sys.argv[i],
-            token = Tokeniser(sys.argv[i])
-            print token.get_tokens()
+            style = Styler(sys.argv[i])
+            print style.errors
 
