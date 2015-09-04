4051887c0eded5f9af1abe0ccb1f061dde185328
SJShaw <SJShaw@users.noreply.github.com>
2015-09-04 17:07:20 +1000

    manual pre-merge of very stale whitespace test source

--- tests/files/whitespace.c
+++ tests/files/whitespace.c
@@ -1,8 +1,66 @@
 #include<stdio.h> //error
+#include <stdlib.h>
 
 int one =1; //error
+int two = 2;
+int three= 3; //error
 
-void f() {
+/* tests spacing around possible unary operators */
+void unary_binary_mix() {
+    int *p = &one;
+    int a = -one;
+    a = - one; //error
+    a = one - two;
+    a = one-two; //error
+    a = *p;
+    a = one * two;
+    a = one*two; //error
+    a = * p; //error
+    p = & one; //error
+    a = +one;
+    a = + one; //error
+    a = one + two;
+    a = one + *p;
+    a = one +*p; //error
+    a = *p + one;
+    a = * p + one; //error
+}
+
+/* tests spacing around binary operators */
+void binary_operators() {
+    int a = 1, b = 1, c = 1;
+    a = b / c;
+    a = b/c; //error
+    a = b/ c; //error
+    a = b /c; //error
+    a = b%c; //error
+    a = b % c;
+    a = b && c;
+    a = b&&c; //error
+    a = b || c;
+    a = b||c; //error
+    a = b ^ c;
+    a = b^c; //error
+    a = b & c;
+    a = b&c; //error
+    a = b | c;
+    a = b|c; //error
+    a = b & c;
+    a = b&c; //error
+    a = b < c;
+    a = b<c; //error
+    a = b <= c;
+    a = b<=c; //error
+
+    struct S {
+        int a;
+    } s, *t;
+    t = &s;
+    a = s.a;
+    a = s . a; //error
+    a = t->a;
+    a = t -> a; //error
+    
 }
 
 int main(){ //error
@@ -15,8 +73,14 @@ int main(){ //error
     }
 
     // A really long statement that has to cover lots of lines=something
-    if (sum == 1|| sum == 2 ||sum == 3 || sum == 4||sum == 5 ||
-            sum == 6 || sum==7 || sum == 8 || sum == 9 || sum == 11 ) { //error
+    if (sum == 1 || sum == 2 
+            ||sum == 3 //error
+            || sum == 4 ||sum == 5 || //error
+            sum == 6 || sum == 7 
+            || sum ==8 //error
+            || sum == 9 || sum == 11 ||
+            sum == 12 || sum ==13 //error
+            || sum== 14 || sum == 15) { //error
         printf("something went %s wrong!\n","terribly"); //error
         printf("oh %s!\n", "noes");
     } else {
@@ -25,6 +89,10 @@ int main(){ //error
     
     if ( i ) { //error
         i++;
+        ++i;
+    } else {
+        ++ i; //error
+        i ++; //error
     }
     int x = 2,y; //error
     switch(x) {
@@ -45,33 +113,5 @@ int main(){ //error
             y-= 1; //error
             break;
     }
-}
-
-void g(int a) {
-    if (a) {
-        a++;        b++; //error, missing newline
-    }
-}
-/*commented func but no vertical whitespace*/
-void h(int a) { //violation for no vertical whitespace
-}
-void j(int a,int b) { //violation for no vertical whitespace AND comma
-}
-
-void pointers() {
-    int *a;
-    int* b; //error for consistency only
-    int * c; //error
-    int * *d; //error for space in pointer types
-    int*e; //error for no spaces
-}
-
-void prototype_check();
-void prototype_check() { //error for no vertical whitespace
-}
-
-
-
-void big_gap() { //error for too much whitespace
 }
 
